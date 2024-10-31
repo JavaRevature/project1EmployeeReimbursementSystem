0710c537177c6ce2690b15c13c6b83f8d68f0df0
Hengrui Jia <hari20020511@gmail.com>
2024-10-30 22:45:30 -0400

    done

---
+++ src/test/java/com/revature/ers_backend/ErsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.ers_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
