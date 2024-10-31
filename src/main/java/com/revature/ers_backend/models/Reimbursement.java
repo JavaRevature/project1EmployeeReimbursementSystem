fa482482c838c3753c22c51a48b702924ec405cc
Hengrui Jia <hari20020511@gmail.com>
2024-10-30 23:51:15 -0400

    role user set up

---
+++ src/main/java/com/revature/ers_backend/models/Reimbursement.java
@@ -0,0 +1,5 @@
+package com.revature.ers_backend.models;
+
+public class Reimbursement {
+
+}
---
+++ src/main/java/com/revature/ers_backend/models/User.java
@@ -0,0 +1,31 @@
+package com.revature.ers_backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.boot.autoconfigure.web.WebProperties.Resources.Chain.Strategy;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+}
