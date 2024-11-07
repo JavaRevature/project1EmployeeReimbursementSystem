b43ca7ff0a7d04e10aa095c5ff73be525cb2b2b8
Hengrui Jia <hari20020511@gmail.com>
2024-11-07 11:33:10 -0500

    added update user

--- src/main/java/com/revature/ers_backend/controllers/UserController.java
+++ src/main/java/com/revature/ers_backend/controllers/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,17 @@ public class UserController {
         else {
             return ResponseEntity.status(200).body(userService.deleteUser(userId));
         }
+    }
+
+    @PatchMapping("/userId")
+    public ResponseEntity<User> updateUser(@RequestBody Role role, @PathVariable int userId,Principal principal) {
+        if(userService.getUserByUsername(principal.getName()).getRole() != Role.MANAGER) {
+            return ResponseEntity.status(403).build();
+        }
+        else{
+            return ResponseEntity.status(200).body(userService.updateUserRole(userId, role));
+        }
+
     }
 
     @GetMapping("/me")
