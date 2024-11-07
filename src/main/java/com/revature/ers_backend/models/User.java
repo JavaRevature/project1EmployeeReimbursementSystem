4301d1fb31e19be6f721df6f4e1cbd37acd135d7
Hengrui Jia <hari20020511@gmail.com>
2024-11-07 11:26:57 -0500

    Removes some unused imports

--- src/main/java/com/revature/ers_backend/models/User.java
+++ src/main/java/com/revature/ers_backend/models/User.java
@@ -14,7 +14,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
--- src/main/java/com/revature/ers_backend/services/JWTService.java
+++ src/main/java/com/revature/ers_backend/services/JWTService.java
@@ -15,7 +15,6 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-import io.jsonwebtoken.Claims;
 
 @Service
 public class JWTService {
